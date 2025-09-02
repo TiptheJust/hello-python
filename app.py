17ce694c771eb386748123eda0fcb3118d1fbcd1
Tyler Colman <tcolman729@gmail.com>
2025-09-01 20:37:43 -0400

    enable Black on save and Ruff autofix

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import requests
 
+
 def get_zen():
     r = requests.get("https://api.github.com/zen", timeout=10)
     return r.text
 
+
 def shout(msg: str) -> str:
     return msg.strip().upper()
+
 
 if __name__ == "__main__":
     text = get_zen()
